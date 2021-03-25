0ee0b3a69386003b9257a1c1df8ccea8dbdcd24b
Undiess <nguyen.andy67@gmail.com>
2021-03-26 01:21:32 +1030

    asd

--- task3.js
+++ task3.js
@@ -2,3 +2,10 @@ var count = 1;
 function counter(element){ 
     element.innerText=count++
 }
+
+function postContent(){ 
+    var time = new Date;
+    var content = document.getElementById("textinput").value;
+
+    var newPost = document.getElementById("posts").innerHTML= "<p>"+time+"</p>  <p>"+content+"</p>"
+}
