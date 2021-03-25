84c96195d7f32bf916948be31c729cd3b1ee0494
Undiess <nguyen.andy67@gmail.com>
2021-03-26 01:25:07 +1030

    asd

--- task3.js
+++ task3.js
@@ -7,5 +7,5 @@ function postContent(){
     var time = new Date;
     var content = document.getElementById("textinput").value;
 
-    document.getElementById("posts").innerHTML= "<p>"+time+"</p>  <p>"+content+"</p>"
+    document.getElementById("posts").innerHTML= "<p class='post-time'>"+time+"</p>  <p class='post-content'>"+content+"</p>"
 }
