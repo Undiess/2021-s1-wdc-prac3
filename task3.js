ca8a3f4211489e81df571c5af93a345888f13916
Undiess <nguyen.andy67@gmail.com>
2021-03-26 01:31:34 +1030

    asd

--- task3.js
+++ task3.js
@@ -9,3 +9,13 @@ function postContent(){
 
     document.getElementById("posts").innerHTML= "<p class='post-time'>"+time+"</p>  <p class='post-content'>"+content+"</p>"
 }
+
+function hidemain(){ 
+    document.getElementById("main").style.display="none"
+    document.getElementById("menu").style.display= "block"
+}
+
+function showmenu(){
+    document.getElementById("menu").style.display="none"
+    document.getElementById("main").style.display= "block"
+}
